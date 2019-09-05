334ddb96858ecdd5ac3fe3d216d5a339b24fee74
Alliyah Blacksher <blacksher@chapman.edu>
2019-09-05 12:34:39 -0700

    first committed project

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello Professor! Hope this semester goes well! Have a goodnight!" << endl;
+	cout << "funfact: I'm using Ubuntu :)" << endl;
+} //cout = print statement for c++
